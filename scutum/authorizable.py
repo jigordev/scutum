5095c845ac4e9307f54e40da009140f81078f97b
jigordev <jigordev@gmail.com>
2025-04-12 20:58:39 -0300

    refactor: update authorizable class decorator

--- scutum/authorizable.py
+++ scutum/authorizable.py
@@ -1,10 +1,12 @@
-def authorizable(cls, gate):
-    def can(self, action: str, *args, **kwargs):
-        return gate.allowed(action, self, *args, **kwargs)
-    
-    def cannot(self, action: str, *args, **kwargs):
-        return gate.denied(action, self, *args, **kwargs)
-    
-    cls.can = can
-    cls.cannot = cannot
-    return cls
+def authorizable(gate):
+    def decorator(cls):
+        def can(self, action: str, *args, **kwargs):
+            return gate.allowed(action, self, *args, **kwargs)
+        
+        def cannot(self, action: str, *args, **kwargs):
+            return gate.denied(action, self, *args, **kwargs)
+        
+        cls.can = can
+        cls.cannot = cannot
+        return cls
+    return decorator
