6459c7ec7bb04281b202c7d22bd2e0d166b143c6
jigordev <jigordev@gmail.com>
2025-05-06 19:04:30 -0300

    feat: support async gate

--- scutum/authorizable.py
+++ scutum/authorizable.py
@@ -1,12 +1,26 @@
-def authorizable(gate):
+from scutum.gate import Gate, AsyncGate
+from typing import Type
+
+def authorizable(gate: Type[Gate] | Type[AsyncGate]):
+    if not isinstance(gate, (Gate, AsyncGate)):
+        raise ValueError("Gate or AsyncGate instance expected")
+
     def decorator(cls):
-        def can(self, action: str, *args, **kwargs):
-            return gate.allowed(action, self, *args, **kwargs)
+        def create_permission_method(method_type: str):
+            async def async_method(self, action: str, *args, **kwargs):
+                if method_type == 'can':
+                    return await gate.allowed(action, self, *args, **kwargs)
+                return await gate.denied(action, self, *args, **kwargs)
+
+            def sync_method(self, action: str, *args, **kwargs):
+                if method_type == 'can':
+                    return gate.allowed(action, self, *args, **kwargs)
+                return gate.denied(action, self, *args, **kwargs)
+
+            return async_method if isinstance(gate, AsyncGate) else sync_method
         
-        def cannot(self, action: str, *args, **kwargs):
-            return gate.denied(action, self, *args, **kwargs)
-        
-        cls.can = can
-        cls.cannot = cannot
+        cls.can = create_permission_method('can')
+        cls.cannot = create_permission_method('cannot')
         return cls
+
     return decorator
